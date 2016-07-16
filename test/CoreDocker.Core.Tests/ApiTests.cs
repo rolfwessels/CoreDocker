40d2eb7c6b640156e5c9f8d1518db0fb1c5b36f8
Rolf Wessels <rolf.wessels@gmail.com>
2016-07-16 18:18:42 +0200

    Add missing test.

---
+++ test/CoreDocker.Core.Tests/ApiTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace CoreDocker.Core.Tests
+{
+    [TestFixture]
+    public class CoreTests
+    {
+        [Test]
+        public void Get_CallsConfig_ShouldReturnConfigValues()
+        {
+//            ILoggerFactory loggerFactory = new LoggerFactory()
+//                .AddConsole()
+//                .AddDebug();
+//            
+
+//            ILogger logger = loggerFactory.CreateLogger<ApiTests>();
+//            FlurlHelper.Log = (t) => logger.LogInformation(t);
+//            var connectionFactory = new ConnectionFactory("http://localhost:5000/");
+//            var connection = connectionFactory.GetConnection();
+//            connection.Configs.Get().Wait();
+        }
+    }
+}
