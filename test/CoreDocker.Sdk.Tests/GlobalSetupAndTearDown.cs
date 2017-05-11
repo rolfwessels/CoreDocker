5f601c96006e4b50b10f9286d53d0be517c9a1ff
Rolf Wessels <rolf@22seven.com>
2017-05-11 09:51:15 +0200

    Move shared logging out.

--- test/CoreDocker.Sdk.Tests/GlobalSetupAndTearDown.cs
+++ test/CoreDocker.Sdk.Tests/GlobalSetupAndTearDown.cs
@@ -1,39 +1,16 @@
-﻿using System.IO;
-using Microsoft.Extensions.Logging;
+﻿using log4net;
 using NUnit.Framework;
-using Serilog;
-using log4net;
-using Serilog.Sinks.RollingFileAlternate;
 
 namespace CoreDocker.Sdk.Tests
 {
-    
-    public class GlobalSetupAndTearDown
+  public class GlobalSetupAndTearDown
+  {
+    private static readonly ILog _log = LogManager.GetLogger<GlobalSetupAndTearDown>();
+
+    [Test]
+    public void method_GiventestingFor_Shouldresult()
     {
-        private static readonly ILog _log = LogManager.GetLogger<GlobalSetupAndTearDown>();
-
-        [SetUp]
-        public void GlobalSetup()
-        {
-            // Do login here.
-            var loggerFactory = new LoggerFactory();
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.RollingFileAlternate(@"C:\temp\logs\CoreDocker.Api", fileSizeLimitBytes: 1024  )
-                .CreateLogger();
-
-            loggerFactory.AddDebug();
-            loggerFactory.AddSerilog();
-
-            LogManager.SetLogger(loggerFactory);
-            Log.Logger.Information("Starting");
-        }
-
-        [TearDown]
-        public void GlobalTeardown()
-        {
-            // Do logout here
-            _log.Info("Dones");
-        }
+      SharedTestLogger.Instance.EnsureEnabled();
     }
+  }
 }
--- testall.ps1
+++ testall.ps1
@@ -1,3 +1,4 @@
 dotnet restore
+dotnet build
 dotnet vstest (Get-ChildItem test | % { Join-Path $_.FullName -ChildPath ("bin/Debug/netcoreapp1.1/$($_.Name).dll") })
 move testresults\*.trx testresults\result.trx
